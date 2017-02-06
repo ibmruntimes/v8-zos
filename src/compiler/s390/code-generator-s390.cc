dfb4b6143ae80c02b308db0cb27ac1cbbc9bea0c
Muntasir Mallick <mmallick@ca.ibm.com>
2017-02-06 11:00:16 -0500

    [z/OS] Function descriptor support

--- src/compiler/s390/code-generator-s390.cc
+++ src/compiler/s390/code-generator-s390.cc
@@ -1786,9 +1786,10 @@ void CodeGenerator::AssembleDeoptimizerCall(
 
 void CodeGenerator::AssemblePrologue() {
   CallDescriptor* descriptor = linkage()->GetIncomingDescriptor();
-
+ 
   if (frame_access_state()->has_frame()) {
     if (descriptor->IsCFunctionCall()) {
+      __ function_descriptor();
       __ Push(r14, fp);
       __ LoadRR(fp, sp);
     } else if (descriptor->IsJSFunctionCall()) {
