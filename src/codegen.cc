ibmruntimes/v8z

[z/OS] Add ebcdic conversions for print all code.
A few more conversions to make annotate sim possible on z/OS.

src/codegen.cc
@@ -213,7 +213,7 @@ void CodeGenerator::PrintCode(Handle<Code> code, CompilationInfo* info) {
             function->end_position() - function->start_position() + 1;
         for (int i = 0; i < source_len; i++) {
           if (stream.HasMore()) {
-            os << AsUC16(stream.GetNext());
+            os << AsUC16(Ascii2Ebcdic(stream.GetNext()));
           }
         }
         os << "\n\n";
@@ -222,8 +222,9 @@ void CodeGenerator::PrintCode(Handle<Code> code, CompilationInfo* info) {
     if (info->IsOptimizing()) {
       if (FLAG_print_unopt_code) {
         os << "--- Unoptimized code ---\n";
-        info->closure()->shared()->code()->Disassemble(
-            function->debug_name()->ToCString().get(), os);
+        char* ebc = function->debug_name()->ToCString().get();
+        __a2e_s(ebc);
+        info->closure()->shared()->code()->Disassemble(ebc, os);
       }
       os << "--- Optimized code ---\n"
          << "optimization_id = " << info->optimization_id() << "\n";

ibmruntimes/v8-zos

Conflicts:
	src/codegen.cc
	src/objects.cc

src/codegen.cc
@@ -174,7 +174,11 @@ void CodeGenerator::PrintCode(Handle<Code> code, CompilationInfo* info) {
             literal->end_position() - literal->start_position() + 1;
         for (int i = 0; i < source_len; i++) {
           if (stream.HasMore()) {
+#ifdef V8_OS_ZOS
+            os << AsReversiblyEscapedUC16(Ascii2Ebcdic(stream.GetNext()));
+#else
             os << AsReversiblyEscapedUC16(stream.GetNext());
+#endif
           }
         }
         os << "\n\n";
@@ -183,7 +187,13 @@ void CodeGenerator::PrintCode(Handle<Code> code, CompilationInfo* info) {
     if (info->IsOptimizing()) {
       if (FLAG_print_unopt_code && info->parse_info()) {
         os << "--- Unoptimized code ---\n";
+#ifdef V8_OS_ZOS
+        char* ebc = debug_name.get();
+        __a2e_s(ebc);
+        info->closure()->shared()->code()->Disassemble(ebc, os);
+#else
         info->closure()->shared()->code()->Disassemble(debug_name.get(), os);
+#endif
       }
       os << "--- Optimized code ---\n"
          << "optimization_id = " << info->optimization_id() << "\n";